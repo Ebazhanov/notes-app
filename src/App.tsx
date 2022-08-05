84cd44253c0122733716d42bbd1add53e4ae39da
ebazhanov <evg.bazhanov@yahoo.com>
2022-08-05 14:13:08 +0200

    add favicon and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App">Note App</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
